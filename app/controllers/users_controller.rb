491802533f179e2c5eaf6b1e3566d0c2e0db5d85
Aaron Chang <xesuschang@gmail.com>
2021-01-03 14:44:51 +0800

    add follow/unfollow features

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :find_user
+
+  def follow
+    friend_id = params.require(:friend_id)
+    f = Friendship.new(user_id: @user.id, friend_id: friend_id)
+
+    unless f.valid?
+      return render json: { error: 'friend not exist' }
+    end
+
+    f.save
+    render json: { mesage: 'ok' }
+  end
+
+  def unfollow
+    friend_id = params.require(:friend_id)
+    f = Friendship.find_by(user_id: @user.id, friend_id: friend_id)
+
+    if f.nil?
+      return render json: { error: 'you are not friends' }
+    end
+
+    f.destroy
+
+    render json: { message: 'ok' }
+  end
+
+  private
+
+  def find_user
+    if params[:user].blank?
+      return render json: { error: 'user id not exist' }
+    end
+
+    begin
+      @user = User.find(params[:user])
+    rescue
+      render json: { error: 'user not exist' }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post '/sleeps', to: 'sleeps#create'
+  post 'users/friends/:friend_id', to: 'users#follow'
+
+  delete '/users/friends/:friend_id', to: 'users#unfollow'
 end
