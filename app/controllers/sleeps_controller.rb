7f39866840365cbbec9bb603f59df7c9127330fb
Aaron Chang <xesuschang@gmail.com>
2021-01-03 14:34:03 +0800

    create sleep

---
+++ app/controllers/sleeps_controller.rb
@@ -0,0 +1,30 @@
+class SleepsController < ApplicationController
+  before_action :find_user
+
+  def create
+    user_id, from, to = params.require([:user, :from, :to])
+    from, to = Time.parse(from), Time.parse(to)
+
+    if from >= to
+      return render json: { error: 'sleep time incorrect, from should be earlier than to'}
+    end
+
+    Sleep.create(user_id: user_id, from: from, to: to, duration: to-from)
+
+    render json: { message: Sleep.list_user(user_id) }
+  end
+
+  private
+
+  def find_user
+    if params[:user].blank?
+      return render json: { error: 'user id not exist' }
+    end
+
+    begin
+      @user = User.find(params[:user])
+    rescue
+      render json: { error: 'user not exist' }
+    end
+  end
+end
--- app/models/sleep.rb
+++ app/models/sleep.rb
@@ -3,4 +3,10 @@ class Sleep < ApplicationRecord
 
   def index
   end
+
+  def self.list_user(user_id)
+    User.find(user_id)
+        .sleeps.order("created_at asc")
+        .select(:from, :to, :duration)
+  end
 end
